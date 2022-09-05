094901651a874f0de44f4237c545ed138d735572
Louis Dijkstra <louisdijkstra (at) gmail.com>
2022-09-05 13:40:52 +0200

    Added link with C, copied from pvm package

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+#' CVN
+#'
+#' An estimator for graphical models changing with multiple discrete
+#' external covariates
+#'
+#' @docType package
+#' @author Louis Dijkstra
+#' @import Rcpp
+#' @useDynLib CVN
+#' @name CVN
+NULL
+
