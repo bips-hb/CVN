57497e5edc7e693049142db6daa908446dff46fd
Lukas Burk <lukas@quantenbrot.de>
2024-02-10 19:35:17 +0100

    More cleanup

--- R/CVN-package.R
+++ R/CVN-package.R
@@ -3,6 +3,12 @@
 
 ## usethis namespace: start
 #' @import Rcpp
+#' @importFrom Matrix Matrix
+#' @importFrom stats cov
+#' @importFrom stats optim
+#' @importFrom stats runif
+#' @importFrom stats var
+#' @importFrom utils combn
 #' @useDynLib CVN
 ## usethis namespace: end
 NULL
--- R/genlasso-wrapper.R
+++ R/genlasso-wrapper.R
@@ -1,9 +1,11 @@
 #' Wrapper for \code{genlassoRcpp}
-#' 
+#'
 #' See for details \code{\link{genlassoRcpp}}
-#' 
+#'
 #' @seealso \code{\link{genlassoRcpp}}
 #' @export
-genlasso_wrapper <- function(y, W, m, c, eta1, eta2, a, rho, max_iter, eps, truncate) { 
-  genlassoRcpp(y, W, m, c, eta1, eta2, a, rho, max_iter, eps, truncate) 
+# FIXME: This passes an addiitonal argument 'c' that is not listed in the definition
+# of genlassoRcpp in CVN.cpp
+genlasso_wrapper <- function(y, W, m, c, eta1, eta2, a, rho, max_iter, eps, truncate) {
+  genlassoRcpp(y, W, m, c, eta1, eta2, a, rho, max_iter, eps, truncate)
 }
